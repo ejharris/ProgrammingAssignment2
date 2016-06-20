bf204ab91fabc450f174cdb03982f0f87d80be27
Emily Harris <ms.emilyharris@icloud.com>
2016-06-20 12:20:15 -0700

    Completion of Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programmed by Emily Harris ProgrammingAssignment2
+## The purpose of program is to write two functions: The first makeCacheMatrix
+## creates an invertible matrix for later computations
+## The second cacheSolve returns the inverse of the cacheMatrix
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a square matrix and saves a matrix object
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
+	inv = NULL
+	set <- function(y) {
 
+		x <<- y
+		inv <<- NULL
+
+
+	]
+
+	get <- function() x
+	setInverse <- function(inverse) inv <<- inverse
+	getInverse <- function() inv
+	list(set = set, get = get, setInverse = setInverse,
+		getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolves returns the inverse of the invertible square matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	inv = x$getInverse()
+	if(!is.null(inv)) {
+
+		message("retrieving cached data")
+		return(inv)
+
+	}
+
+	matrixA <-x$get()
+	inv <- solve(matrixA,...)
+	x$setInverse(inv)
+	inv
 }
+
+}
+
+
